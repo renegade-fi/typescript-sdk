44f559a686dce284a4b411fa0f64b05ce613578b
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-05-03 15:15:50 -0700

    core: Prepare pay fees task to queue withdraw right after

--- packages/core/src/actions/withdraw.ts
+++ packages/core/src/actions/withdraw.ts
@@ -27,16 +27,6 @@ export async function withdraw(
   const walletId = getWalletId(config)
   const wallet = await getBackOfQueueWallet(config)
 
-  // Pay Fees
-  // try {
-  //     if (shouldPayFees) {
-  //         await payFees(config)
-  //     }
-  // } catch (error) {
-  //     console.error(`Failed to pay fees before withdrawing, cancelling withdraw.`)
-  //     throw error
-  // }
-
   // Withdraw
   const body = utils.withdraw(
     JSONBigInt.stringify(wallet),
