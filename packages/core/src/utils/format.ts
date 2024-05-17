21f63aa1e96d2b22d175a6695fbb4d5d86cd9549
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-05-16 23:16:36 -0700

    core: allow for dynamic decimal truncation

--- packages/core/src/utils/format.ts
+++ packages/core/src/utils/format.ts
@@ -2,13 +2,11 @@ import { formatUnits, parseUnits } from 'viem'
 
 import type { Token } from '../types/token.js'
 
-export function formatAmount(amount: bigint, token: Token) {
-  const decimals = token.decimals
-  if (!decimals) throw new Error(`Decimals not found for 0x${token.address}`)
-  let formatted = formatUnits(amount, decimals)
+export function formatAmount(amount: bigint, token: Token, decimals = 2) {
+  let formatted = formatUnits(amount, token.decimals)
   if (formatted.includes('.')) {
     const [integerPart, decimalPart = ''] = formatted.split('.')
-    formatted = `${integerPart}.${decimalPart.substring(0, 2)}`
+    formatted = `${integerPart}.${decimalPart.substring(0, decimals)}`
   }
   return formatted
 }
