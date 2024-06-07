9332bd10d41b2f13d7f3861fadbe4440aaca57bd
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-06 18:54:42 -0700

    react: proxy exports

--- packages/react/src/hooks/useOrderHistoryWebSocket.ts
+++ packages/react/src/hooks/useOrderHistoryWebSocket.ts
@@ -4,6 +4,7 @@ import {
   RENEGADE_AUTH_HEADER_NAME,
   RENEGADE_SIG_EXPIRATION_HEADER_NAME,
   WS_WALLET_ORDERS_ROUTE,
+  getSkRoot,
   parseBigJSON,
   type Config,
   type OrderMetadata,
@@ -11,8 +12,8 @@ import {
 import { useEffect } from 'react'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
 import { useConfig } from './useConfig.js'
-
-import { getSkRoot, useStatus, useWalletId } from '../index.js'
+import { useStatus } from './useStatus.js'
+import { useWalletId } from './useWalletId.js'
 
 export type UseOrderHistoryWebSocketParameters = {
   config?: Config
