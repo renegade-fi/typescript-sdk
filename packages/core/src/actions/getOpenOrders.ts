bc04c1bac0d6d58f819f2e8c3c77fd3d79fcfabd
Andrew Kirillov <andrew@kirillov.org>
2024-07-22 17:14:15 -0700

    core: actions: getOpenOrders: add pool query param

--- packages/core/src/actions/getOpenOrders.ts
+++ packages/core/src/actions/getOpenOrders.ts
@@ -1,25 +1,34 @@
 import type { Config } from '../createConfig.js'
-import type { OrderMetadata } from '../types/order.js'
+import type { OpenOrder } from '../types/order.js'
 import { ADMIN_OPEN_ORDERS_ROUTE } from '../constants.js'
 import { getRelayerWithAdmin } from '../utils/http.js'
 import { BaseError, type BaseErrorType } from '../errors/base.js'
 
-export type GetOpenOrdersReturnType = Map<string, OrderMetadata>
+export type GetOpenOrdersParams = {
+  matchingPool?: string
+}
+
+export type GetOpenOrdersReturnType = Map<string, OpenOrder>
 
 export type GetOpenOrdersErrorType = BaseErrorType
 
 export async function getOpenOrders(
   config: Config,
+  parameters: GetOpenOrdersParams = {},
 ): Promise<GetOpenOrdersReturnType> {
   const { getRelayerBaseUrl } = config
 
-  const res = await getRelayerWithAdmin(
-    config,
-    getRelayerBaseUrl(ADMIN_OPEN_ORDERS_ROUTE),
-  )
+  let url = getRelayerBaseUrl(ADMIN_OPEN_ORDERS_ROUTE)
+  if (parameters.matchingPool) {
+    const temp = new URL(url)
+    temp.searchParams.set('matching_pool', parameters.matchingPool)
+    url = temp.toString()
+  }
+
+  const res = await getRelayerWithAdmin(config, url)
 
   if (!res.orders) {
     throw new BaseError('No orders found')
   }
-  return new Map(res.orders.map((order: OrderMetadata) => [order.id, order]))
+  return new Map(res.orders.map((order: OpenOrder) => [order.order.id, order]))
 }
