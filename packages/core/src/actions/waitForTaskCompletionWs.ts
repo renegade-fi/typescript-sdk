90090dfaca5d466daf805e417b27c65be33e75b5
Andrew Kirillov <andrew@kirillov.org>
2024-08-15 16:38:14 -0700

    core: actions: waitForTaskCompletionWs: use task history in prefetch

--- packages/core/src/actions/waitForTaskCompletionWs.ts
+++ packages/core/src/actions/waitForTaskCompletionWs.ts
@@ -1,10 +1,9 @@
-import axios from 'axios'
 import type { Config } from '../createConfig.js'
-import { getTaskStatus } from './getTaskStatus.js'
 import type { WaitForTaskCompletionParameters } from './waitForTaskCompletion.js'
 import type { RelayerWebsocketMessage } from '../types/ws.js'
 import { TASK_STATUS_ROUTE } from '../constants.js'
 import { websocketWaiter } from '../utils/websocketWaiter.js'
+import { getTaskHistory } from './getTaskHistory.js'
 
 export async function waitForTaskCompletionWs(
   config: Config,
@@ -12,19 +11,18 @@ export async function waitForTaskCompletionWs(
 ): Promise<null | undefined> {
   const { id, timeout } = parameters
 
-  const url = config.getWebsocketBaseUrl()
   const topic = TASK_STATUS_ROUTE(id)
 
   const prefetch = async () => {
-    try {
-      await getTaskStatus(config, { id })
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        if (error.response?.status === 404) {
-          // Assume a 404 means the task is completed
-          return null
-        }
-      }
+    const taskHistory = await getTaskHistory(config)
+    const task = taskHistory.get(id)
+
+    if (task?.state === 'Completed') {
+      return null
+    }
+
+    if (task?.state === 'Failed') {
+      throw new Error(`Task ${id} failed`)
     }
 
     return undefined
@@ -44,5 +42,5 @@ export async function waitForTaskCompletionWs(
     return undefined
   }
 
-  return websocketWaiter(config, url, topic, messageHandler, prefetch, timeout)
+  return websocketWaiter(config, topic, messageHandler, prefetch, timeout)
 }
