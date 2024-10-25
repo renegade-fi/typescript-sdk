f4bb455abc1cf320211b527ebad8f145eac73130
Andrew Kirillov <andrew@kirillov.org>
2024-10-25 15:22:40 -0700

    core: actions: update getWalletOrderIds to getWalletMatchableOrderIds

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -175,9 +175,9 @@ export const ADMIN_ASSIGN_ORDER_ROUTE = (
 // Route to get the matching pool for an order
 export const ADMIN_GET_ORDER_MATCHING_POOL_ROUTE = (order_id: string) =>
   `/admin/orders/${order_id}/matching-pool`
-// Route to get all the order IDs for a given wallet
-export const ADMIN_WALLET_ORDER_IDS_ROUTE = (wallet_id: string) =>
-  `/admin/wallet/${wallet_id}/order-ids`
+// Route to get all the matchable order IDs for a given wallet
+export const ADMIN_WALLET_MATCHABLE_ORDER_IDS_ROUTE = (wallet_id: string) =>
+  `/admin/wallet/${wallet_id}/matchable-order-ids`
 
 // The admin wallet updates topic, streams opaque event indicating
 // updates for all wallets
