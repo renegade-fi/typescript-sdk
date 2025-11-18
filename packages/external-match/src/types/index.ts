98a1664be883fab54d767d8c8bf289bbd4caa369
Yun <41171808+sehyunc@users.noreply.github.com>
2025-11-17 17:04:22 -0800

    external-match: request stringified numbers from auth server

--- packages/external-match/src/types/index.ts
+++ packages/external-match/src/types/index.ts
@@ -72,10 +72,69 @@ export interface SignedGasSponsorshipInfo {
     signature: string;
 }
 
-export interface SignedExternalQuote {
+export class SignedExternalQuote {
     quote: ApiExternalQuote;
     signature: string;
     gas_sponsorship_info?: SignedGasSponsorshipInfo;
+
+    constructor(
+        quote: ApiExternalQuote,
+        signature: string,
+        gas_sponsorship_info?: SignedGasSponsorshipInfo,
+    ) {
+        this.quote = quote;
+        this.signature = signature;
+        this.gas_sponsorship_info = gas_sponsorship_info;
+    }
+
+    static deserialize(data: ExternalQuoteResponse): SignedExternalQuote {
+        const quote = data.signed_quote.quote;
+
+        return new SignedExternalQuote(
+            {
+                order: quote.order,
+                match_result: {
+                    quote_mint: quote.match_result.quote_mint,
+                    base_mint: quote.match_result.base_mint,
+                    quote_amount: BigInt(quote.match_result.quote_amount),
+                    base_amount: BigInt(quote.match_result.base_amount),
+                    direction: quote.match_result.direction,
+                },
+                fees: {
+                    relayer_fee: BigInt(quote.fees.relayer_fee),
+                    protocol_fee: BigInt(quote.fees.protocol_fee),
+                },
+                send: {
+                    mint: quote.send.mint,
+                    amount: BigInt(quote.send.amount),
+                },
+                receive: {
+                    mint: quote.receive.mint,
+                    amount: BigInt(quote.receive.amount),
+                },
+                price: {
+                    price: quote.price.price,
+                    timestamp: BigInt(quote.price.timestamp),
+                },
+                timestamp: BigInt(quote.timestamp),
+            },
+            data.signed_quote.signature,
+            data.gas_sponsorship_info
+                ? {
+                      gas_sponsorship_info: {
+                          refund_amount: BigInt(
+                              data.gas_sponsorship_info.gas_sponsorship_info.refund_amount,
+                          ),
+                          refund_native_eth:
+                              data.gas_sponsorship_info.gas_sponsorship_info.refund_native_eth,
+                          refund_address:
+                              data.gas_sponsorship_info.gas_sponsorship_info.refund_address,
+                      },
+                      signature: data.gas_sponsorship_info.signature,
+                  }
+                : undefined,
+        );
+    }
 }
 
 export interface SettlementTransaction {
@@ -116,10 +175,55 @@ export interface ExternalMatchRequest {
     external_order: ExternalOrder;
 }
 
-export interface ExternalMatchResponse {
+export class ExternalMatchResponse {
     match_bundle: AtomicMatchApiBundle;
     gas_sponsored: boolean;
     gas_sponsorship_info?: GasSponsorshipInfo;
+
+    constructor(
+        match_bundle: AtomicMatchApiBundle,
+        gas_sponsored: boolean,
+        gas_sponsorship_info?: GasSponsorshipInfo,
+    ) {
+        this.match_bundle = match_bundle;
+        this.gas_sponsored = gas_sponsored;
+        this.gas_sponsorship_info = gas_sponsorship_info;
+    }
+
+    static deserialize(data: any): ExternalMatchResponse {
+        return new ExternalMatchResponse(
+            {
+                match_result: {
+                    quote_mint: data.match_bundle.match_result.quote_mint,
+                    base_mint: data.match_bundle.match_result.base_mint,
+                    quote_amount: BigInt(data.match_bundle.match_result.quote_amount),
+                    base_amount: BigInt(data.match_bundle.match_result.base_amount),
+                    direction: data.match_bundle.match_result.direction,
+                },
+                fees: {
+                    relayer_fee: BigInt(data.match_bundle.fees.relayer_fee),
+                    protocol_fee: BigInt(data.match_bundle.fees.protocol_fee),
+                },
+                receive: {
+                    mint: data.match_bundle.receive.mint,
+                    amount: BigInt(data.match_bundle.receive.amount),
+                },
+                send: {
+                    mint: data.match_bundle.send.mint,
+                    amount: BigInt(data.match_bundle.send.amount),
+                },
+                settlement_tx: data.match_bundle.settlement_tx,
+            },
+            data.gas_sponsored,
+            data.gas_sponsorship_info
+                ? {
+                      refund_amount: BigInt(data.gas_sponsorship_info.refund_amount),
+                      refund_native_eth: data.gas_sponsorship_info.refund_native_eth,
+                      refund_address: data.gas_sponsorship_info.refund_address,
+                  }
+                : undefined,
+        );
+    }
 }
 
 export interface DepthSideInfo {
@@ -127,11 +231,33 @@ export interface DepthSideInfo {
     total_quantity_usd: number;
 }
 
-export interface OrderBookDepth {
+export class OrderBookDepth {
     price: number;
     timestamp: number;
     buy: DepthSideInfo;
     sell: DepthSideInfo;
+
+    constructor(price: number, timestamp: number, buy: DepthSideInfo, sell: DepthSideInfo) {
+        this.price = price;
+        this.timestamp = timestamp;
+        this.buy = buy;
+        this.sell = sell;
+    }
+
+    static deserialize(data: any): OrderBookDepth {
+        return new OrderBookDepth(
+            Number(data.price),
+            Number(data.timestamp),
+            {
+                total_quantity: BigInt(data.buy.total_quantity),
+                total_quantity_usd: Number(data.buy.total_quantity_usd),
+            },
+            {
+                total_quantity: BigInt(data.sell.total_quantity),
+                total_quantity_usd: Number(data.sell.total_quantity_usd),
+            },
+        );
+    }
 }
 
 export interface ApiToken {
